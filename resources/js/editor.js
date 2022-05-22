114de31fd247aa366d8553167925865ee6baa8fd
Fumiya-Kai <bunjyo.science@icloud.com>
2022-05-22 22:29:40 +0900

    clean: コメント修正

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -1,5 +1,6 @@
 import { marked } from 'marked';
 
+// タブの切り替わり機能
 $(function(){
   $('.nav-link').on('click', function(){
     $('.nav-link.active').removeClass('active');
@@ -13,10 +14,13 @@ $(function(){
   let oldHeight = $('.article-content').innerHeight();
 
   $('.article-content').on('input', function(){
+
+    // テキストエリアの高さが自動で変わる機能
     $('.article-content').innerHeight(oldHeight);
     let height = $(this).get(0).scrollHeight;
     $('.article-content').innerHeight(height);
 
+    // マークダウンのコンパイル機能
     let compiledMarkdown = marked($(this).val());
     $('.preview').html(compiledMarkdown);
   });
