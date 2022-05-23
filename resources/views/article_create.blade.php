c2b97ca148af88aa9a0736d8a5a248e4b6d781bd
Fumiya-Kai <bunjyo.science@icloud.com>
2022-05-23 22:06:03 +0900

    fix: 読み込み順整理

--- resources/views/article_create.blade.php
+++ resources/views/article_create.blade.php
@@ -65,12 +65,11 @@
     </div>
   </form>
 </div>
-<script src="{{ mix('js/comboBox.js') }}"></script>
-
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/darcula.min.css">
 <script type="text/javascript" async
     src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML">
 </script>
+<script src="{{ mix('js/comboBox.js') }}"></script>
 <script src="{{ mix('js/editor.js') }}"></script>
 
 @endsection
