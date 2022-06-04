eea6c66ad4c802d11244a21dd597ba95e81f23a0
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-04 18:14:20 +0900

    add: teamモデルにuseridでチームを取得するメソッド追加

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -9,11 +9,20 @@ class Team extends Model
 {
     use HasFactory;
 
+    const TEAMS_PER_PAGE = 10;
+
     protected $fillable = [
         'name',
         'description',
         'document',
     ];
+
+    public function getByUserId($id)
+    {
+        return $this->where('user_id', $id)
+                    ->latest()
+                    ->paginate(self::TEAMS_PER_PAGE);
+    }
 
     public function users()
     {
