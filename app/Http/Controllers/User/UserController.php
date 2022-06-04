61b4bdf4dde535ee0346c8f0b17975ee7d22316b
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-04 17:39:09 +0900

    fix: mypageコントローラーの名前変更

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class MypageController extends Controller
+class UserController extends Controller
 {
     //
 }
