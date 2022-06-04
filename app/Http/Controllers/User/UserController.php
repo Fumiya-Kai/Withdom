5fb10f1d2548b656857aad927f25a0f81b551af4
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-04 17:54:45 +0900

    add: コントローラーにマイページ遷移のメソッドを作成

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\Team;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    private $user;
+    private $article;
+    private $team;
+
+    public function __construct(User $user, Article $article, Team $team)
+    {
+      $this->user = $user;
+      $this->article = $article;
+      $this->team = $team;
+    }
+
+    public function mypage()
+    {
+      $teams = $this->team->getByUserId(Auth::id());
+      $articles = $this->article->getByUserId(Auth::id());
+
+      return view('mypage', compact('teams', 'articles'));
+    }
 }
