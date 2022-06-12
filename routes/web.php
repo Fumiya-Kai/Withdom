e4779dc5283256be657539c74bf64348b706712b
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-12 20:54:45 +0900

    fix: ルーティングを修正

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/mypage', [App\Http\Controllers\User\UserController::class, 'mypage'])->name('mypage');
 
 Route::group(['middleware' => 'auth.team.session'], function() {
-    Route::resource('team', App\Http\Controllers\User\TeamController::class)->except('show', 'create');
+    Route::resource('team', App\Http\Controllers\User\TeamController::class)->except('show', 'create', 'store');
     Route::resource('article', App\Http\Controllers\User\ArticleController::class);
 });
 
+Route::resource('team', App\Http\Controllers\User\TeamController::class)->only('create', 'store');
 Route::get('/team/{teamId}', [App\Http\Controllers\User\TeamController::class, 'show'])->middleware('auth.team.route')->name('team.show');
 
 Auth::routes();
