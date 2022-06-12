92ebb54f4ced4eb9567ee652bee85237488602a5
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-12 19:58:38 +0900

    fix: チームidのセッションにハッシュ値を追加

--- app/Http/Middleware/TeamAuthenticate.php
+++ app/Http/Middleware/TeamAuthenticate.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class TeamAuthenticate
 {
@@ -24,7 +25,12 @@ class TeamAuthenticate
                 if($request->session()->has('team_id')) {
                     $request->session()->forget('key');
                 }
-                session(['team_id' => $teamId]);
+                session([
+                    'team_id' => [
+                        'id' => $teamId,
+                        'check' => Hash::make('team_id='. $teamId)
+                    ]
+                ]);
                 return $next($request);
             }
         }
