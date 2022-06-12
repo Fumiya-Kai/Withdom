beb27996b4bdb59b237a1ccc70824305848a009b
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-12 16:46:21 +0900

    add: 記事作成のフォームリクエスト作成

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "categories" => 'exists:categories,id',
+            'title' => 'required|max:255',
+            'abstract' => 'required|max:255',
+            'content' => 'required|max:5000',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => '入力必須の項目です。',
+            'max' => ':max文字以内で入力してください。',
+            'exists' => '不正なカテゴリが含まれています',
+        ];
+    }
+}
