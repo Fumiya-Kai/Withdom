1e2d992e4c46a8f688166d79e900479ffea6df27
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-12 16:47:06 +0900

    add: 記事作成コントローラーにバリデーションの追加

--- app/Http/Controllers/User/ArticleController.php
+++ app/Http/Controllers/User/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ArticleRequest;
 use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -24,11 +25,13 @@ class ArticleController extends Controller
         return view('article_create', compact('categories'));
     }
 
-    public function store(Request $request)
+    public function store(ArticleRequest $request)
     {
+        $input = $request->validated();
         $teamId = $request->session()->get('team_id');
-        $newCategoriesInput = $request->only('new-categories');
-        $newArticleInput = $request->except('new-categories');
+        $newCategoriesInput = $input['new-categories'];
+        unset($input['new-categories']);
+        $newArticleInput = $input;
         $newCategoryIds = $this->category->saveNewAndGetIds($newCategoriesInput);
         $this->article->saveNewArticle($newArticleInput, $newCategoryIds, $teamId);
     }
