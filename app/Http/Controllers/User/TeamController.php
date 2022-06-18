19f84cace5695e9b20f7fddbefbab9fd9ae47193
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-18 22:30:45 +0900

    fix: チーム作成時の招待機能のコントローラーを追加

--- app/Http/Controllers/User/TeamController.php
+++ app/Http/Controllers/User/TeamController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TeamRequest;
+use App\Mail\StudyTeamInvitation;
 use App\Models\Article;
 use App\Models\Team;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class TeamController extends Controller
 {
@@ -31,7 +33,15 @@ class TeamController extends Controller
     public function store(TeamRequest $request)
     {
         $input = $request->validated();
-        $this->team->createTeam($input, Auth::id());
+        $teamId = $this->team->createTeamAndGetId($input, Auth::id());
+        $this->inviteToNewTeam($input['emails'], Auth::user()->name, $input['name'], $teamId);
         return redirect()->route('mypage');
+    }
+
+    private function inviteToNewTeam($mailTo, $fromName, $teamName, $teamId)
+    {
+        foreach($mailTo as $address) {
+            Mail::to($address)->send( new StudyTeamInvitation($fromName, $teamName, $teamId) );
+        }
     }
 }
