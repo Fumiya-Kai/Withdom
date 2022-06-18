2c848edce36c614c2c77adeded90b556ae815dc8
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-18 22:23:50 +0900

    fix: 招待時のログイン機能の追加

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,32 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function showInvitedLogin(Request $request)
+    {
+        if (! $request->hasValidSignature()) {
+            abort(401);
+        }
+        $teamId = $request->team_id;
+        $signature = Hash::make('team_id='. $teamId);
+        return view('auth.invited.login', compact('teamId', 'signature'));
+    }
+
+    public function authenticated(Request $request, $user)
+    {
+        $input = $request->only('team_id', 'signature');
+        if($request->path() === 'login_invited') {
+            if(Hash::check('team_id='. $input['team_id'], $input['signature'])) {
+                $user->teams()->sync($input['team_id']);
+                return redirect()->route('team.show');
+            } else {
+                abort(401);
+            }
+        } elseif($request->path() === 'login') {
+            return redirect()->route('mypage');
+        } else {
+            abort(401);
+        }
     }
 }
