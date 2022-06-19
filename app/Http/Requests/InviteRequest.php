8e96808ac274998f019caebcd7f4bafa56750171
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-19 17:36:48 +0900

    add: メンバー招待のフォームリクエストを追加

---
+++ app/Http/Requests/InviteRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\isEmailAddress;
+use App\Rules\uniqueEmailAddress;
+use Illuminate\Foundation\Http\FormRequest;
+
+class InviteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        $postData = $this->all();
+        return [
+            'emails' => empty($postData['emails'][0])? '' : [new isEmailAddress, new uniqueEmailAddress],
+        ];
+    }
+}
