018e28affe1ea1326261bb690c1dfc41b2dc2312
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-25 21:52:13 +0900

    fix: メンバー招待画面にFormファサードを導入

--- resources/views/invite.blade.php
+++ resources/views/invite.blade.php
@@ -5,22 +5,36 @@
   <div class="row">
     <div class="display-6">チーム作成</div>
   </div>
-  <form action="{{ route('invite.mail') }}" method="post" class="mt-5">
-    @csrf
+  {{ Form::open(['route' => 'invite.mail', 'method' => 'POST', 'class' => 'tm-5']) }}
+    {{ Form::token() }}
     <div class="add-members w-50 mt-4">
-      <label for="email" class="form-label h5">ユーザー招待</label>
-      <input type="email" id="email0" name="emails[]" class="form-control @if($errors->has('emails')) is-invalid @endif" placeholder="招待するユーザーのメールアドレスを入力">
+      {{ Form::label('email', 'ユーザー招待', ['class' => 'form-label h5']) }}
+      @if(!!old('emails'))
+      @foreach(old('emails') as $email)
+      {{ Form::email('emails[]',
+                     $email,
+                     $errors->has('emails') ? ['class' => 'form-control mt-2 is-invalid', 'id' => 'email0', 'placeholder' => '招待するユーザーのメールアドレスを入力']
+                                             : ['class' => 'form-control mt-2', 'id' => 'email0', 'placeholder' => '招待するユーザーのメールアドレスを入力'])
+      }}
+      @endforeach
+      @else
+      {{ Form::email('emails[]',
+                     null,
+                     $errors->has('emails') ? ['class' => 'form-control is-invalid', 'id' => 'email0', 'placeholder' => '招待するユーザーのメールアドレスを入力']
+                                             : ['class' => 'form-control', 'id' => 'email0', 'placeholder' => '招待するユーザーのメールアドレスを入力'])
+      }}
+      @endif
       <span class="invalid-feedback">{{ $errors->first('emails') }}</span>
     </div>
     <div class="mt-2">
-      <button type="button" class="btn-add-form btn btn-success py-0 px-1">
-        <img src="https://icongr.am/material/plus.svg?size=30&color=ffffff" alt="招待メンバー追加">
-      </button>
+      {{ Form::button('<img src="https://icongr.am/material/plus.svg?size=30&color=ffffff" alt="招待メンバー追加">',
+                      ['class' => 'btn-add-form btn btn-success py-0 px-1'])
+      }}
     </div>
     <div class="row mt-3">
-      <button class="submit btn btn-warning col-1 offset-5">作成</button>
+      {{ Form::submit('作成', ['class' => 'submit btn btn-warning col-1 offset-5']) }}
     </div>
-  </form>
+  {{ Form::close() }}
 </div>
 <script src="{{ mix('js/addForm.js') }}"></script>
 @endsection
