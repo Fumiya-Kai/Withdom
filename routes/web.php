25098070e966ed56cbde5c6ea71d1f85f9a2328b
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-25 23:59:54 +0900

    fix: authミドルウェアを適用

--- routes/web.php
+++ routes/web.php
@@ -12,20 +12,21 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/mypage', [App\Http\Controllers\User\UserController::class, 'mypage'])->name('mypage');
 
-Route::get('/mypage', [App\Http\Controllers\User\UserController::class, 'mypage'])->name('mypage');
+    Route::group(['middleware' => 'auth.team.session'], function() {
+        Route::resource('team', App\Http\Controllers\User\TeamController::class)->except('show', 'create', 'store');
+        Route::resource('article', App\Http\Controllers\User\ArticleController::class);
+    });
 
-Route::group(['middleware' => 'auth.team.session'], function() {
-    Route::resource('team', App\Http\Controllers\User\TeamController::class)->except('show', 'create', 'store');
-    Route::resource('article', App\Http\Controllers\User\ArticleController::class);
+    Route::resource('team', App\Http\Controllers\User\TeamController::class)->only('create', 'store');
+    Route::get('/team/{teamId}', [App\Http\Controllers\User\TeamController::class, 'show'])->middleware('auth.team.route')->name('team.show');
+    Route::get('/login_invited', [App\Http\Controllers\Auth\LoginController::class, 'showInvitedLogin'])->name('login.invited');
+    Route::get('/register_invited', [App\Http\Controllers\Auth\RegisterController::class, 'showInvitedRegister'])->name('register.invited');
+    Route::get('/invite', [App\Http\Controllers\User\TeamController::class, 'showInviteForm'])->name('invite.form');
+    Route::post('/invite', [App\Http\Controllers\User\TeamController::class, 'inviteToExistingTeam'])->name('invite.mail');
 });
-
-Route::resource('team', App\Http\Controllers\User\TeamController::class)->only('create', 'store');
-Route::get('/team/{teamId}', [App\Http\Controllers\User\TeamController::class, 'show'])->middleware('auth.team.route')->name('team.show');
-Route::get('/login_invited', [App\Http\Controllers\Auth\LoginController::class, 'showInvitedLogin'])->name('login.invited');
-Route::get('/register_invited', [App\Http\Controllers\Auth\RegisterController::class, 'showInvitedRegister'])->name('register.invited');
-Route::get('/invite', [App\Http\Controllers\User\TeamController::class, 'showInviteForm'])->name('invite.form');
-Route::post('/invite', [App\Http\Controllers\User\TeamController::class, 'inviteToExistingTeam'])->name('invite.mail');
 
 Auth::routes(['register' => false]);
 Route::post('/register_invited', [App\Http\Controllers\Auth\RegisterController::class, 'showInvitedRegister'])->name('register');
