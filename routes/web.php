b557bd7a661145dd518a37933ac00118c5142dea
Fumiya-Kai <bunjyo.science@icloud.com>
2022-06-29 23:01:34 +0900

    fix: 認証関連はauthミドルウェアを適用しないように修正

--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,13 @@ Route::group(['middleware' => 'auth'], function() {
 
     Route::resource('team', App\Http\Controllers\User\TeamController::class)->only('create', 'store');
     Route::get('/team/{teamId}', [App\Http\Controllers\User\TeamController::class, 'show'])->middleware('auth.team.route')->name('team.show');
-    Route::get('/login_invited', [App\Http\Controllers\Auth\LoginController::class, 'showInvitedLogin'])->name('login.invited');
-    Route::get('/register_invited', [App\Http\Controllers\Auth\RegisterController::class, 'showInvitedRegister'])->name('register.invited');
     Route::get('/invite', [App\Http\Controllers\User\TeamController::class, 'showInviteForm'])->name('invite.form');
     Route::post('/invite', [App\Http\Controllers\User\TeamController::class, 'inviteToExistingTeam'])->name('invite.mail');
 });
 
 Auth::routes(['register' => false]);
-Route::post('/register_invited', [App\Http\Controllers\Auth\RegisterController::class, 'showInvitedRegister'])->name('register');
+Route::get('/login_invited', [App\Http\Controllers\Auth\LoginController::class, 'showInvitedLogin'])->name('login.invited');
+Route::get('/register_invited', [App\Http\Controllers\Auth\RegisterController::class, 'showInvitedRegister'])->name('register.invited');
+Route::post('/register_invited', [App\Http\Controllers\Auth\RegisterController::class, 'register'])->name('register');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
